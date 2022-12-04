44fd4891767df23ca08ad49e368e0d0a80e3bfda
Ramon Franco <rfmoney2015@gmail.com>
2022-12-04 18:12:04 -0300

    Agregando Seccion de Productos, por Ramon

--- loja-pets/src/constants/images.js
+++ loja-pets/src/constants/images.js
@@ -1,7 +1,13 @@
-import background from '../assets/petsback.jpg';
+import background from '../assets/petsback.png';
+import comida from '../assets/comida.png';
+import casa from '../assets/casa.png';
+import shapoo from '../assets/shapoo.png';
 
 const images = {
   background,
+  casa,
+  comida,
+  shapoo,
 };
 
-export { images };
+export { images, casa, comida, shapoo };
