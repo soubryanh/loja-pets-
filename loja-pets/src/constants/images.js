4dcddaccf7cdb1abf4c8730224eff58547515f62
Annelys Vivenes <annelys.scarleth@gmail.com>
2022-12-04 20:56:03 -0300

    agregando secion de servicios, annelys

--- loja-pets/src/constants/images.js
+++ loja-pets/src/constants/images.js
@@ -3,7 +3,7 @@ import comida from '../assets/comida.png';
 import casa from '../assets/casa.png';
 import shapoo from '../assets/shapoo.png';
 import barbinha from '../assets/Barbinha0.png';
-
+import barbinhas from '../assets/Barbinha.png';
 
 const images = {
   background,
@@ -11,6 +11,8 @@ const images = {
   comida,
   shapoo,
   barbinha,
+  barbinhas,
 };
 
-export { images, casa, comida, shapoo, barbinha};
+export { images, casa, comida, shapoo, barbinha,
+barbinhas};
