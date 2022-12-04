1add31e9e49378b5b7d0deec006ff5e2245c6a63
antonio gonga <antoniovicentegonga@gmail.com>
2022-12-04 20:09:00 -0300

    estou agregando a secção de header e nav, António Vicente

--- loja-pets/src/constants/images.js
+++ loja-pets/src/constants/images.js
@@ -2,12 +2,15 @@ import background from '../assets/petsback.png';
 import comida from '../assets/comida.png';
 import casa from '../assets/casa.png';
 import shapoo from '../assets/shapoo.png';
+import barbinha from '../assets/Barbinha0.png';
+
 
 const images = {
   background,
   casa,
   comida,
   shapoo,
+  barbinha,
 };
 
-export { images, casa, comida, shapoo };
+export { images, casa, comida, shapoo, barbinha};
