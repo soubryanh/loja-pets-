3a9cb370bc07fb3b525575580f7ae322264c824e
NeudimarP <neudimarp@gmail.com>
2022-12-04 22:56:54 -0300

    agregando seccion de about, neudimar

--- loja-pets/src/components/About/About.js
+++ loja-pets/src/components/About/About.js
@@ -1,5 +1,27 @@
+import { images } from '../../constants';
+import './About.css';
+
 function About() {
-  return <div className='About'></div>;
+  return (
+    <>
+      <main id='about'>
+        <section className='about-section'>
+          <div className='content-section'>
+            <h2>Sobre nós</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+              dolore ullam necessitatibus adipisci ea nam atque consectetur vel.
+              Modi, beatae ratione nam consectetur voluptatem animi amet quidem
+              dqv-uakv-xqcsequi expedita, blanditiis doloribus quisquam architecto?
+            </p>
+          </div>
+          <aside className='image'>
+            <img src={images.dogs} alt='dogs' />
+          </aside>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default About;
--- loja-pets/src/constants/images.js
+++ loja-pets/src/constants/images.js
@@ -4,6 +4,7 @@ import casa from '../assets/casa.png';
 import shapoo from '../assets/shapoo.png';
 import barbinha from '../assets/Barbinha0.png';
 import barbinhas from '../assets/Barbinha.png';
+import dogs from '../assets/dogs.png';
 
 const images = {
   background,
@@ -12,7 +13,8 @@ const images = {
   shapoo,
   barbinha,
   barbinhas,
+  dogs,
 };
 
 export { images, casa, comida, shapoo, barbinha,
-barbinhas};
+barbinhas, dogs};
